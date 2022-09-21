8eb80d22a4838054d173c2c4d73a3207d17ddbd1
jkhbuild <jkh.build@gmail.com>
2022-09-21 09:13:31 -0400

    battleship project part 1

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,28 @@
+require "byebug"
 class Board
+
+    attr_reader :size
+    def initialize(n)
+        @grid = Array.new(n) {Array.new(n, :N)}
+        @size = (n * n)
+    end
+
+    def [](pos)
+        @grid[pos[0]][pos[-1]]
+    end
+
+    def []=(pos, value)
+        @grid[pos[0]][pos[-1]] = value
+    end
+
+    def num_ships
+        count = 0
+        @grid.each do |row|
+            row.each { |pos| count += 1 if pos == :S }
+        end
+        count
+    end
+
+
   
 end
