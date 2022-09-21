96788d587ab4238cf0a3847483fa609e4d06e8cd
jkhbuild <jkh.build@gmail.com>
2022-09-21 16:20:15 -0400

    tic tac toe v1 checkpoint 2 board complete

--- Tic Tac Toe_v1/board.rb
+++ Tic Tac Toe_v1/board.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Board
 
     def initialize
@@ -24,4 +25,87 @@ class Board
             raise "invalid mark"
         end
     end
+
+    def print_board
+        @grid.each_with_index do |row, i1|
+            row.each_with_index do |value, i2|
+                if i2 < row.length - 1
+                    print value.to_s + " "
+                else
+                    print value.to_s
+                    print "\n"
+                end
+            end
+        end
+    end
+
+    def win_row?(mark)
+        @grid.each { |row| return true if row.all?(mark)}
+        false
+    end
+
+    def win_col?(mark)
+        @grid.transpose.each { |col| return true if col.all?(mark) }
+        false
+    end
+
+    def win_diagonal?(mark)
+
+        ltr = [0, 0]
+        ltr_values = []
+        y = ltr[0]
+        x = ltr[-1]
+
+        while self.valid?([x, y]) #to check left to right diagonal
+            ltr_values << @grid[y][x]
+
+            x += 1
+            y += 1
+        end
+
+        return true if ltr_values.all?(mark)
+
+        rtl = [0, @grid.length-1]
+        rtl_values = []
+        y = rtl[0]
+        x = rtl[-1]
+
+        while self.valid?([x, y]) #to check right to left diagonal
+            rtl_values << @grid[y][x]
+
+            x -= 1
+            y += 1
+        end
+
+        return true if rtl_values.all?(mark)
+        false
+    end
+
+    def win?(mark)
+        if win_row?(mark)
+            return true
+        elsif win_col?(mark)
+            return true
+        elsif win_diagonal?(mark)
+            return true
+        end
+        false
+    end
+
+    def empty_positions?
+        @grid.each do |row|
+            row.each do |value|
+                return true if value == "_"
+            end
+        end
+        false
+    end
 end
+
+b = Board.new
+
+b.place_mark([0,0], :X)
+b.place_mark([1,1], :X)
+b.place_mark([2,2], :X)
+b.win_diagonal?(:X)
+print b.print_board
