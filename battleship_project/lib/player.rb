4e393438ee5e251382723653d512ee289896d1c0
jkhbuild <jkh.build@gmail.com>
2022-09-21 11:33:41 -0400

    battleship part 3(player)

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,4 +1,11 @@
 class Player
+    def get_move
+        print "enter a position with coordinates seperated with a space like '4, 7'"
+        input = gets.chomp
 
-
+        numbers = "0123456789"
+        input_arr = []
+        input.each_char {|ele| input_arr << ele.to_i if numbers.include?(ele)}
+        input_arr
+    end
 end
