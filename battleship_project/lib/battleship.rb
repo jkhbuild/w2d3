ef690a7a7b1eee85fe0c428d3d80f1b3b44dae1a
jkhbuild <jkh.build@gmail.com>
2022-09-21 11:56:41 -0400

    battleship part 3(battleship)

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -2,8 +2,52 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-    def initialize
+    attr_reader :board, :player
+    def initialize(n)
+        @player = Player.new
+        @board = Board.new(n)
+        @remaining_misses = @board.size / 2
+    end
 
+    def start_game
+        @board.place_random_ships
+        puts @board.num_ships
+        @board.print
+    end
+
+    def lose?
+        if @remaining_misses > 0
+            return false
+        else
+            print "you lose"
+            true
+        end
+    end
+
+    def win?
+        if @board.num_ships > 0
+            return false
+        else
+            print "you win"
+            true
+        end
+    end
+
+    def game_over?
+        if self.win? || self.lose?
+            return true
+        else
+            false
+        end
+    end
+
+    def turn
+        if !@board.attack(@player.get_move)
+            @remaining_misses -= 1
+        end
+
+        @board.print
+        print @remaining_misses
     end
 
 end
