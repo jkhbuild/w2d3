16d7e1d807a03227abd8ee2c3b7fd9dc3273c70a
jkhbuild <jkh.build@gmail.com>
2022-09-21 13:57:31 -0400

    tic tac toe v1 checkpoint1

--- Tic Tac Toe_v1/board.rb
+++ Tic Tac Toe_v1/board.rb
@@ -1,1 +1,27 @@
-jhj
+class Board
+
+    def initialize
+        @grid = Array.new(3){Array.new(3, "_")}
+    end
+
+    def valid?(coordinates)
+        coordinates.each { |cord| return false if cord < 0 || cord > @grid.length - 1 }
+        true
+    end
+
+    def empty?(coordinates)
+        y = coordinates[0]
+        x = coordinates[-1]
+        @grid[y][x] == "_"
+    end
+
+    def place_mark(coordinates, symbol)
+        y = coordinates[0]
+        x = coordinates[-1]
+        if self.empty?(coordinates) && self.valid?(coordinates)
+            @grid[y][x] = symbol
+        else
+            raise "invalid mark"
+        end
+    end
+end
