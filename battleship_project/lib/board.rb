10d07af863691d5105cd1f8e8e382504643da0c3
jkhbuild <jkh.build@gmail.com>
2022-09-21 11:21:24 -0400

    battleship project part 2

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,7 +1,7 @@
 require "byebug"
 class Board
 
-    attr_reader :size
+    attr_reader :size, :grid
     def initialize(n)
         @grid = Array.new(n) {Array.new(n, :N)}
         @size = (n * n)
@@ -23,6 +23,59 @@ class Board
         count
     end
 
+    def attack(pos)
+        if self[pos] == :S
+            self[pos] = :H
+            puts "you sunk my battleship!"
+            return true
+        else
+            self[pos] = :X
+            return false
+        end
+    end
 
-  
+    def place_random_ships
+        number_of_ships = @grid.flatten.length / 4
+        n = @grid.length - 1
+
+        while @grid.flatten.count(:S) < number_of_ships
+            random_pos = [rand(0..n-1), rand(0..n-1)]
+            self[random_pos] = :S
+        end
+    end
+
+    def hidden_ships_grid
+        n = @grid.length
+        hidden_grid = Array.new(n) {Array.new(n, :N)}
+        @grid.each_with_index do |row, i1|
+            row.each_with_index do |value, i2|
+                if value != :S
+                    hidden_grid[i1][i2] = value
+                end
+            end
+        end
+        hidden_grid
+    end
+
+    def self.print_grid(grid)
+        grid_print_output = ""
+        grid.each_with_index do |row, i1|
+            row.each_with_index do |value, i2|
+                if i2 < row.length - 1
+                    print value.to_s + " "
+                else
+                    print value.to_s
+                    print "\n"
+                end
+            end
+        end
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(self.hidden_ships_grid)
+    end
 end
